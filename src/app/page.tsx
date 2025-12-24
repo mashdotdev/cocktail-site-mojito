f613b44da99b2f0a9a3c248da55dec900d901c1c
SyedMash <mashrtx7@gmail.com>
2025-12-24 11:53:28 +0500

    implement about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/about";
 import Cocktails from "@/components/cocktails";
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
@@ -8,6 +9,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
